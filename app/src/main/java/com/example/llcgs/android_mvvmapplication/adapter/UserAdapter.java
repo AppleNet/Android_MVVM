67eaccd7fa2123c01342691229efe99ea9620a08
liulongchao <liulongchao@gomeholdings.com>
2017-05-22 16:44:26 +0800

    Android_MVVM_DataBinding---RecyclerView

---
+++ app/src/main/java/com/example/llcgs/android_mvvmapplication/adapter/UserAdapter.java
@@ -0,0 +1,68 @@
+package com.example.llcgs.android_mvvmapplication.adapter;
+
+import android.databinding.DataBindingUtil;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import com.example.llcgs.android_mvvmapplication.R;
+import com.example.llcgs.android_mvvmapplication.databinding.ViewItemUserBinding;
+import com.example.llcgs.android_mvvmapplication.model.LoginBean;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * com.example.llcgs.android_mvvmapplication.adapter.UserAdapter
+ *
+ * @author liulongchao
+ * @since 2017/5/22
+ */
+
+
+public class UserAdapter extends RecyclerView.Adapter {
+
+    private List<LoginBean> list;
+
+    public UserAdapter() {
+        list = Collections.emptyList();
+    }
+
+    public void setList(List<LoginBean> list) {
+        this.list = list;
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        ViewItemUserBinding viewItemUserBinding = DataBindingUtil.inflate(LayoutInflater.from(parent.getContext()), R.layout.view_item_user, parent, false);
+        return new UserViewHolder(viewItemUserBinding);
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        ((UserViewHolder)holder).bindUser(list.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+
+    static class UserViewHolder extends RecyclerView.ViewHolder{
+
+        ViewItemUserBinding viewItemUserBinding;
+
+
+        public UserViewHolder(ViewItemUserBinding viewItemUserBinding) {
+            super(viewItemUserBinding.itemUser);
+            this.viewItemUserBinding = viewItemUserBinding;
+        }
+
+        void bindUser(LoginBean loginBean){
+            viewItemUserBinding.setItemLoginBean(loginBean);
+        }
+
+    }
+
+}
--- app/src/main/java/com/example/llcgs/android_mvvmapplication/view/SecondActivity.java
+++ app/src/main/java/com/example/llcgs/android_mvvmapplication/view/SecondActivity.java
@@ -4,10 +4,15 @@ import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 
 import com.example.llcgs.android_mvvmapplication.R;
+import com.example.llcgs.android_mvvmapplication.adapter.UserAdapter;
 import com.example.llcgs.android_mvvmapplication.databinding.ActivitySecondBinding;
 import com.example.llcgs.android_mvvmapplication.model.LoginBean;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * com.example.llcgs.android_mvvmapplication.view.SecondActivity
@@ -31,5 +36,15 @@ public class SecondActivity extends AppCompatActivity {
         activitySecondBinding = DataBindingUtil.setContentView(this, R.layout.activity_second);
         LoginBean loginBean = getIntent().getParcelableExtra("loginBean");
         activitySecondBinding.setLoginBean(loginBean);
+
+        UserAdapter userAdapter = new UserAdapter();
+        List<LoginBean> list = new ArrayList<>();
+        for (int i=0; i<=10; i++){
+            list.add(loginBean);
+        }
+        userAdapter.setList(list);
+        activitySecondBinding.recyclerview.setAdapter(userAdapter);
+        activitySecondBinding.recyclerview.setLayoutManager(new LinearLayoutManager(this));
     }
+
 }
